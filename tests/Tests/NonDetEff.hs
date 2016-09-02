b2be496989b1d86d12f5f488eada1cb553952391
joshvera <josh@joshvera.com>
2016-09-02 15:38:13 -0400

    Export NonDetEff

--- tests/Tests/NonDetEff.hs
+++ tests/Tests/NonDetEff.hs
@@ -4,6 +4,7 @@ module Tests.NonDetEff where
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Freer
+import Control.Monad.Freer.NonDetEff
 
 ifte :: (NonDetEff :< r)
      => Eff r a -> (a -> Eff r b) -> Eff r b -> Eff r b
