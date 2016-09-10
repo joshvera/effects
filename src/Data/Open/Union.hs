3b9460629cdfc1d7890e32d5a44e502bbd52e516
joshvera <josh@joshvera.com>
2016-09-10 15:13:10 -0400

    More docs

--- src/Data/Open/Union.hs
+++ src/Data/Open/Union.hs
@@ -20,7 +20,7 @@ Portability : POSIX
 
 All operations are constant-time, and there is no Typeable constraint
 
-This is a variation of OpenUion5.hs, which relies on overlapping
+This is a variation of OpenUnion5.hs, which relies on overlapping
 instances instead of closed type families. Closed type families
 have their problems: overlapping instances can resolve even
 for unground types, but closed type families are subject to a
@@ -74,7 +74,7 @@ prj' :: Int -> Union r v -> Maybe (t v)
 prj' n (Union n' x) | n == n'   = Just (unsafeCoerce x)
                     | otherwise = Nothing
 
-newtype P t r = P{unP :: Int}
+newtype P t r = P { unP :: Int }
 
 infixr 5 :<:
 -- | Find a list of members 'm' in an open union 'r'.
@@ -114,9 +114,8 @@ decompose0 (Union _ v) = Right $ unsafeCoerce v
 weaken :: Union r w -> Union (any ': r) w
 weaken (Union n v) = Union (n+1) v
 
--- Find an index of an element in a `list'
--- The element must exist
--- This is essentially a compile-time computation.
+-- Find an index of an element in an `r'.
+-- The element must exist, so this is essentially a compile-time computation.
 class FindElem (t :: * -> *) r where
   elemNo :: P t r
 
