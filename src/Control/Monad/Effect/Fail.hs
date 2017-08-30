da86b801fde0d290f281846e1f4e9fb40e94c5e4
Rob Rix <rob.rix@me.com>
2017-08-30 15:17:11 -0400

    Add a MonadFail instance.

--- src/Control/Monad/Effect/Fail.hs
+++ src/Control/Monad/Effect/Fail.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE DataKinds, TypeOperators #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, TypeOperators, UndecidableInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Control.Monad.Effect.Fail
 ( Fail
 , runFail
@@ -6,8 +7,13 @@ module Control.Monad.Effect.Fail
 
 import Control.Monad.Effect
 import Control.Monad.Effect.Internal
+import Control.Monad.Fail
 
 data Fail a = Fail { failMessage :: String }
 
 runFail :: Eff (Fail ': fs) a -> Eff fs (Either String a)
 runFail = relay (pure . Right) (const . pure . Left . failMessage)
+
+
+instance Fail :< fs => MonadFail (Eff fs) where
+  fail = send . Fail
