fc6be7dddc87a937f561074ca5734fa37a93cab5
Rob Rix <rob.rix@me.com>
2017-08-30 15:15:39 -0400

    Add a Fail interpreter.

--- src/Control/Monad/Effect/Fail.hs
+++ src/Control/Monad/Effect/Fail.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE DataKinds, TypeOperators #-}
 module Control.Monad.Effect.Fail
 ( Fail
+, runFail
 ) where
 
-data Fail a = Fail String
+import Control.Monad.Effect
+import Control.Monad.Effect.Internal
+
+data Fail a = Fail { failMessage :: String }
+
+runFail :: Eff (Fail ': fs) a -> Eff fs (Either String a)
+runFail = relay (pure . Right) (const . pure . Left . failMessage)
