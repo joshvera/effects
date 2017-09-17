813ab2ffb4db705ec8ea6915e8bbbac17c2e2b84
Rob Rix <rob.rix@me.com>
2017-09-17 19:04:34 -0400

    Bind union in the where clause.

--- src/Data/Union/Templates.hs
+++ src/Data/Union/Templates.hs
@@ -11,12 +11,9 @@ mkApplyInstance paramN =
   InstanceD Nothing (AppT constraint <$> typeParams) (AppT (AppT (ConT applyC) constraint) (foldr (AppT . AppT PromotedConsT) PromotedNilT typeParams))
     [ FunD apply (zipWith mkClause [0..] typeParams) ]
   where typeParams = VarT . mkName . ('f' :) . show <$> [0..pred paramN]
-        [applyC, apply, f, r] = mkName <$> ["Apply", "apply", "f", "r"]
+        [applyC, apply, f, r, union] = mkName <$> ["Apply", "apply", "f", "r", "union"]
         [constraint, a] = VarT . mkName <$> ["constraint", "a"]
         mkClause i nthType = Clause
           [ WildP, VarP f, ConP union [ LitP (IntegerL i), VarP r ] ]
           (NormalB (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r)) (AppT nthType a))))
           []
-
-union :: Name
-union = mkName "Union"
