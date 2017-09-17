2ad81da9e94a37f7a308f3b7f33be38ca065ee5b
Rob Rix <rob.rix@me.com>
2017-09-17 13:44:52 -0400

    Generate the apply functions outside of the monad.

--- src/Data/Union/Templates.hs
+++ src/Data/Union/Templates.hs
@@ -15,37 +15,31 @@ mkApplyInstance :: Integer -> Q [Dec]
 mkApplyInstance paramN = do
   c <- newName "c"
   typeParams <- map VarT <$> replicateM (fromIntegral paramN) (newName "f")
-  applyD <- mkApplyFunction typeParams
-  apply2D <- mkApply2Function typeParams
   pure
     [ InstanceD Nothing (AppT (VarT c) <$> typeParams) (AppT (AppT (ConT apply) (VarT c)) (foldr (AppT . AppT PromotedConsT) PromotedNilT typeParams))
-      (applyD ++ apply2D)
+      [mkApplyFunction typeParams, mkApply2Function typeParams]
     ]
   where apply = mkName "Apply"
 
-mkApplyFunction :: [Type] -> Q [Dec]
+mkApplyFunction :: [Type] -> Dec
 mkApplyFunction typeParams =
-  pure
-    [ FunD apply
-      [ Clause
-        [ WildP, VarP f, ConP union [ VarP n, VarP r ] ]
-        (NormalB (CaseE (VarE n) (zipWith mkMatch [0..] typeParams)))
-        []
-      ]
+  FunD apply
+    [ Clause
+      [ WildP, VarP f, ConP union [ VarP n, VarP r ] ]
+      (NormalB (CaseE (VarE n) (zipWith mkMatch [0..] typeParams)))
+      []
     ]
   where mkMatch i nthType = Match (LitP (IntegerL i)) (NormalB (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r)) (AppT nthType (VarT a))))) []
         [apply, f, n, r, a] = mkName <$> ["apply", "f", "n", "r", "a"]
 
 
-mkApply2Function :: [Type] -> Q [Dec]
+mkApply2Function :: [Type] -> Dec
 mkApply2Function typeParams =
-  pure
-    [ FunD apply2
-      [ Clause
-        [ WildP, VarP f, ConP union [ VarP n1, VarP r1 ], ConP union [ VarP n2, VarP r2 ] ]
-        (NormalB (CaseE (TupE [ VarE n1, VarE n2 ]) ((zipWith mkMatch [0..] typeParams) ++ [ Match WildP (NormalB (ConE 'Nothing)) [] ])))
-        []
-      ]
+  FunD apply2
+    [ Clause
+      [ WildP, VarP f, ConP union [ VarP n1, VarP r1 ], ConP union [ VarP n2, VarP r2 ] ]
+      (NormalB (CaseE (TupE [ VarE n1, VarE n2 ]) ((zipWith mkMatch [0..] typeParams) ++ [ Match WildP (NormalB (ConE 'Nothing)) [] ])))
+      []
     ]
   where mkMatch i nthType = Match (TupP [LitP (IntegerL i), LitP (IntegerL i)]) (NormalB (AppE (ConE 'Just) (AppE (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r1)) (AppT nthType (VarT a)))) (AppE (VarE 'unsafeCoerce) (VarE r2))))) []
         [apply2, f, n1, n2, r1, r2, a] = mkName <$> ["apply2", "f", "n1", "n2", "r1", "r2", "a"]
