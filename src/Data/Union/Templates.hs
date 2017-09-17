9c3b3d8233af5c8202ea5b49c48d827739d748e4
Rob Rix <rob.rix@me.com>
2017-09-17 13:36:47 -0400

    Pass the mkApply{2,}Function functions lists of type parameters.

--- src/Data/Union/Templates.hs
+++ src/Data/Union/Templates.hs
@@ -14,39 +14,39 @@ mkApplyInstances = fmap concat . traverse mkApplyInstance
 mkApplyInstance :: Integer -> Q [Dec]
 mkApplyInstance paramN = do
   c <- newName "c"
-  params <- replicateM (fromIntegral paramN) (newName "f")
-  applyD <- mkApplyFunction params
-  apply2D <- mkApply2Function params
+  typeParams <- map VarT <$> replicateM (fromIntegral paramN) (newName "f")
+  applyD <- mkApplyFunction typeParams
+  apply2D <- mkApply2Function typeParams
   pure
-    [ InstanceD Nothing (AppT (VarT c) . VarT <$> params) (AppT (AppT (ConT apply) (VarT c)) (foldr (AppT . AppT PromotedConsT . VarT) PromotedNilT params))
+    [ InstanceD Nothing (AppT (VarT c) <$> typeParams) (AppT (AppT (ConT apply) (VarT c)) (foldr (AppT . AppT PromotedConsT) PromotedNilT typeParams))
       (applyD ++ apply2D)
     ]
   where apply = mkName "Apply"
 
-mkApplyFunction :: [Name] -> Q [Dec]
-mkApplyFunction paramNames = do
-  [f, n, r, a] <- traverse newName ["f", "n", "r", "a"]
-  let mkMatch n' = Match (LitP (IntegerL n')) (NormalB (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r)) (AppT (VarT (paramNames !! fromIntegral n')) (VarT a))))) []
+mkApplyFunction :: [Type] -> Q [Dec]
+mkApplyFunction typeParams = do
   pure
     [ FunD apply
       [ Clause
         [ WildP, VarP f, ConP union [ VarP n, VarP r ] ]
-        (NormalB (CaseE (VarE n) (mkMatch <$> [0..pred (fromIntegral (length paramNames))])))
+        (NormalB (CaseE (VarE n) (uncurry mkMatch <$> zip [0..] typeParams)))
         []
       ]
     ]
   where apply = mkName "apply"
+        mkMatch i nthType = Match (LitP (IntegerL i)) (NormalB (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r)) (AppT nthType (VarT a))))) []
+        (f, n, r, a) = (mkName "f", mkName "n", mkName "r", mkName "a")
 
 
-mkApply2Function :: [Name] -> Q [Dec]
-mkApply2Function paramNames  = do
+mkApply2Function :: [Type] -> Q [Dec]
+mkApply2Function typeParams  = do
   [f, n1, n2, r1, r2, a] <- traverse newName ["f", "n1", "n2", "r1", "r2", "a"]
-  let mkMatch n = Match (TupP [LitP (IntegerL n), LitP (IntegerL n)]) (NormalB (AppE (ConE 'Just) (AppE (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r1)) (AppT (VarT (paramNames !! fromIntegral n)) (VarT a)))) (AppE (VarE 'unsafeCoerce) (VarE r2))))) []
+  let mkMatch n = Match (TupP [LitP (IntegerL n), LitP (IntegerL n)]) (NormalB (AppE (ConE 'Just) (AppE (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r1)) (AppT (typeParams !! fromIntegral n) (VarT a)))) (AppE (VarE 'unsafeCoerce) (VarE r2))))) []
   pure
     [ FunD apply2
       [ Clause
         [ WildP, VarP f, ConP union [ VarP n1, VarP r1 ], ConP union [ VarP n2, VarP r2 ] ]
-        (NormalB (CaseE (TupE [ VarE n1, VarE n2 ]) ((mkMatch <$> [0..pred (fromIntegral (length paramNames))]) ++ [ Match WildP (NormalB (ConE 'Nothing)) [] ])))
+        (NormalB (CaseE (TupE [ VarE n1, VarE n2 ]) ((mkMatch <$> [0..pred (fromIntegral (length typeParams))]) ++ [ Match WildP (NormalB (ConE 'Nothing)) [] ])))
         []
       ]
     ]
