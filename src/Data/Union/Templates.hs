ed66c721079a5d407864b61e7f4a200198e80e49
Rob Rix <rob.rix@me.com>
2017-09-17 18:52:15 -0400

    Simplify the apply template to make multiple clauses.

--- src/Data/Union/Templates.hs
+++ src/Data/Union/Templates.hs
@@ -9,22 +9,14 @@ import Unsafe.Coerce (unsafeCoerce)
 mkApplyInstance :: Integer -> Dec
 mkApplyInstance paramN =
   InstanceD Nothing (AppT constraint <$> typeParams) (AppT (AppT (ConT applyC) constraint) (foldr (AppT . AppT PromotedConsT) PromotedNilT typeParams))
-    [ FunD apply
-      [ Clause
-        [ WildP, VarP f, ConP union [ VarP n, VarP r ] ]
-        (NormalB (CaseE (VarE n)
-          (  zipWith mkMatch [0..] typeParams
-          ++ [ Match WildP (NormalB (AppE (VarE 'error)
-            (InfixE (Just (LitE (StringL "index ")))
-            (VarE '(++)) (Just (InfixE (Just (AppE (VarE 'show) (VarE n)))
-            (VarE '(++)) (Just (LitE (StringL (" out of union bounds (" ++ show (length typeParams) ++ ")"))))))))) [] ])))
-        []
-      ]
-    ]
+    [ FunD apply (zipWith mkClause [0..] typeParams) ]
   where typeParams = VarT . mkName . ('f' :) . show <$> [0..pred paramN]
-        [applyC, apply, f, n, r] = mkName <$> ["Apply", "apply", "f", "n", "r"]
+        [applyC, apply, f, r] = mkName <$> ["Apply", "apply", "f", "r"]
         [constraint, a] = VarT . mkName <$> ["constraint", "a"]
-        mkMatch i nthType = Match (LitP (IntegerL i)) (NormalB (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r)) (AppT nthType a)))) []
+        mkClause i nthType = Clause
+          [ WildP, VarP f, ConP union [ LitP (IntegerL i), VarP r ] ]
+          (NormalB (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r)) (AppT nthType a))))
+          []
 
 union :: Name
 union = mkName "Union"
