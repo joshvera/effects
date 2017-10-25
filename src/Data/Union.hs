3d74b11718fc6b98827817bcdacbc755843f4c92
Rob Rix <rob.rix@me.com>
2017-10-25 18:04:48 -0400

    Revert ":fire: TypeLits."
    This reverts commit a0554e55787a86a8b411c6a6f2e86b6a2ef83c7a.

--- src/Data/Union.hs
+++ src/Data/Union.hs
@@ -60,6 +60,7 @@ import Data.Union.Templates
 import Unsafe.Coerce(unsafeCoerce)
 import GHC.Exts (Constraint)
 import GHC.Prim (Proxy#, proxy#)
+import GHC.TypeLits
 
 infixr 5 :<
 
@@ -123,27 +124,18 @@ decompose0 (Union _ v) = Right $ unsafeCoerce v
 weaken :: Union r w -> Union (any ': r) w
 weaken (Union n v) = Union (n+1) v
 
-type (t :< r) = Elem (ElemIndex t r)
-
-class Elem (n :: N) where
-  elemNo' :: Proxy# n -> Int
-
-instance Elem 'Z where
-  elemNo' _ = 0
-
-instance Elem n => Elem ('S n) where
-  elemNo' _ = 1 + elemNo' (proxy# :: Proxy# n)
+type (t :< r) = KnownNat (ElemIndex t r)
 
 -- Find an index of an element in an `r'.
 -- The element must exist, so this is essentially a compile-time computation.
 elemNo :: forall t r. (t :< r) => P t r
-elemNo = P (elemNo' (proxy# :: Proxy# (ElemIndex t r)))
+elemNo = P (fromIntegral (natVal' (proxy# :: Proxy# (ElemIndex t r))))
 
-data N = Z | S N
+type ElemIndex t ts = ElemIndex' 0 t ts
 
-type family ElemIndex (t :: * -> *) (ts :: [* -> *]) :: N where
-  ElemIndex t (t ': _) = 'Z
-  ElemIndex t (_ ': ts) = 'S (ElemIndex t ts)
+type family ElemIndex' (n :: Nat) (t :: * -> *) (ts :: [* -> *]) :: Nat where
+  ElemIndex' n t (t ': _) = n
+  ElemIndex' n t (_ ': ts) = ElemIndex' (1 + n) t ts
 
 -- | Helper to apply a function to a functor of the nth type in a type list.
 class Apply (c :: (* -> *) -> Constraint) (fs :: [* -> *]) where
@@ -171,7 +163,7 @@ type family EQU (a :: * -> *) (b :: * -> *) :: Bool where
 
 -- This class is used for emulating monad transformers
 class (t :< r) => MemberU2 (tag :: (* -> *) -> * -> *) (t :: * -> *) r | tag r -> t
-instance (t1 :< r, MemberU' (EQU t1 t2) tag t1 (t2 ': r)) => MemberU2 tag t1 (t2 ': r)
+instance (KnownNat (ElemIndex t1 (t2 ': r)), t1 :< r, MemberU' (EQU t1 t2) tag t1 (t2 ': r)) => MemberU2 tag t1 (t2 ': r)
 
 class (t :< r) =>
       MemberU' (f::Bool) (tag :: (* -> *) -> * -> *) (t :: * -> *) r | tag r -> t
