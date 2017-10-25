e85a51141180973515b823186e3d5418541755a3
Rob Rix <rob.rix@me.com>
2017-10-25 18:34:38 -0400

    Generate (:<) instances using a template.

--- src/Data/Union/Templates.hs
+++ src/Data/Union/Templates.hs
@@ -1,14 +1,23 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Data.Union.Templates
-( mkApplyInstance
+( mkElemInstances
+, mkApplyInstance
 ) where
 
 import Language.Haskell.TH
 import Unsafe.Coerce (unsafeCoerce)
 
+mkElemInstances :: Integer -> [Dec]
+mkElemInstances paramN = mkInstance <$> [0..pred paramN]
+  where mkInstance t = InstanceD Nothing [] (InfixT (mkT t) elemC (typeListT typeParams))
+          [ FunD elemNo [ Clause [] (NormalB (AppE (ConE p) (LitE (IntegerL t)))) [] ] ]
+        mkT = VarT . mkName . ('f' :) . show
+        typeParams = mkT <$> [0..pred paramN]
+        [elemC, elemNo, p] = mkName <$> [":<", "elemNo", "P"]
+
 mkApplyInstance :: Integer -> Dec
 mkApplyInstance paramN =
-  InstanceD Nothing (AppT constraint <$> typeParams) (AppT (AppT (ConT applyC) constraint) (foldr (AppT . AppT PromotedConsT) PromotedNilT typeParams))
+  InstanceD Nothing (AppT constraint <$> typeParams) (AppT (AppT (ConT applyC) constraint) (typeListT typeParams))
     [ FunD apply (zipWith mkClause [0..] typeParams) ]
   where typeParams = VarT . mkName . ('f' :) . show <$> [0..pred paramN]
         [applyC, apply, f, r, union] = mkName <$> ["Apply", "apply", "f", "r", "Union"]
@@ -17,3 +26,6 @@ mkApplyInstance paramN =
           [ WildP, VarP f, ConP union [ LitP (IntegerL i), VarP r ] ]
           (NormalB (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r)) (AppT nthType a))))
           []
+
+typeListT :: [Type] -> Type
+typeListT = foldr (AppT . AppT PromotedConsT) PromotedNilT
