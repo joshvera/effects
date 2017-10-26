c925a08eb1f8c82629a1a31b5ce54fa47518d60e
Rob Rix <rob.rix@me.com>
2017-10-26 11:21:07 -0400

    Split out a type list helper.

--- src/Data/Union/Templates.hs
+++ src/Data/Union/Templates.hs
@@ -8,7 +8,7 @@ import Unsafe.Coerce (unsafeCoerce)
 
 mkApplyInstance :: Integer -> Dec
 mkApplyInstance paramN =
-  InstanceD Nothing (AppT constraint <$> typeParams) (AppT (AppT (ConT applyC) constraint) (foldr (AppT . AppT PromotedConsT) PromotedNilT typeParams))
+  InstanceD Nothing (AppT constraint <$> typeParams) (AppT (AppT (ConT applyC) constraint) (typeListT PromotedNilT typeParams))
     [ FunD apply (zipWith mkClause [0..] typeParams) ]
   where typeParams = VarT . mkName . ('f' :) . show <$> [0..pred paramN]
         [applyC, apply, f, r, union] = mkName <$> ["Apply", "apply", "f", "r", "Union"]
@@ -17,3 +17,6 @@ mkApplyInstance paramN =
           [ WildP, VarP f, ConP union [ LitP (IntegerL i), VarP r ] ]
           (NormalB (AppE (VarE f) (SigE (AppE (VarE 'unsafeCoerce) (VarE r)) (AppT nthType a))))
           []
+
+typeListT :: Type -> [Type] -> Type
+typeListT = foldr (AppT . AppT PromotedConsT)
