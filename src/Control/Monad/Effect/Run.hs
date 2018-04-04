9f9217789ca6df50da139ec78bcea6ad1f9c9de8
Rob Rix <rob.rix@me.com>
2018-04-03 20:51:56 -0400

    Define a case for Fail effects.

--- src/Control/Monad/Effect/Run.hs
+++ src/Control/Monad/Effect/Run.hs
@@ -4,6 +4,7 @@ module Control.Monad.Effect.Run where
 import Control.Monad.Effect.Coroutine
 import Control.Monad.Effect.Embedded
 import Control.Monad.Effect.Exception
+import Control.Monad.Effect.Fail
 import Control.Monad.Effect.Internal as Eff
 
 class Run effects result function | effects result -> function where
@@ -17,6 +18,9 @@ instance Run effects result rest => Run (Embedded effects ': effects) result res
 
 instance Run effects (Either exc result) rest => Run (Exc exc ': effects) result rest where
   run' = run' . runError
+
+instance Run effects (Either String result) rest => Run (Fail ': effects) result rest where
+  run' = run' . runFail
 
 instance Run '[] result result where
   run' = Eff.run
