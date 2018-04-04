e6a3512f54cc2fe699be99cdc0955ba3c967059e
Rob Rix <rob.rix@me.com>
2018-04-03 20:55:49 -0400

    Define a case for Resumable effects.

--- src/Control/Monad/Effect/Run.hs
+++ src/Control/Monad/Effect/Run.hs
@@ -3,12 +3,13 @@ module Control.Monad.Effect.Run where
 
 import Control.Monad.Effect.Coroutine
 import Control.Monad.Effect.Embedded
-import Control.Monad.Effect.Exception
+import Control.Monad.Effect.Exception as Exc
 import Control.Monad.Effect.Fail
 import Control.Monad.Effect.Fresh
 import Control.Monad.Effect.Internal as Eff
 import Control.Monad.Effect.NonDet
 import Control.Monad.Effect.Reader
+import Control.Monad.Effect.Resumable as Resumable
 
 class Run effects result function | effects result -> function where
   run' :: Eff effects result -> function
@@ -20,7 +21,7 @@ instance Run effects result rest => Run (Embedded effects ': effects) result res
   run' = run' . relay pure (\ (Embed e) k -> e >>= k)
 
 instance Run effects (Either exc result) rest => Run (Exc exc ': effects) result rest where
-  run' = run' . runError
+  run' = run' . Exc.runError
 
 instance Run effects (Either String result) rest => Run (Fail ': effects) result rest where
   run' = run' . runFail
@@ -33,6 +34,9 @@ instance (Run effects [result] rest) => Run (NonDet ': effects) result rest wher
 
 instance Run effects result rest => Run (Reader b ': effects) result (b -> rest) where
   run' = fmap run' . runReader
+
+instance Run effects (Either (SomeExc exc) result) rest => Run (Resumable exc ': effects) result rest where
+  run' = run' . Resumable.runError
 
 instance Run '[] result result where
   run' = Eff.run
