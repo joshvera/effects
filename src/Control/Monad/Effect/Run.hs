acd8ab56bad81eebcce3979fbe21b1bdfd030a2e
Rob Rix <rob.rix@me.com>
2018-04-03 20:53:19 -0400

    Define a case for Fresh effects.

--- src/Control/Monad/Effect/Run.hs
+++ src/Control/Monad/Effect/Run.hs
@@ -5,6 +5,7 @@ import Control.Monad.Effect.Coroutine
 import Control.Monad.Effect.Embedded
 import Control.Monad.Effect.Exception
 import Control.Monad.Effect.Fail
+import Control.Monad.Effect.Fresh
 import Control.Monad.Effect.Internal as Eff
 
 class Run effects result function | effects result -> function where
@@ -21,6 +22,9 @@ instance Run effects (Either exc result) rest => Run (Exc exc ': effects) result
 
 instance Run effects (Either String result) rest => Run (Fail ': effects) result rest where
   run' = run' . runFail
+
+instance Run effects result rest => Run (Fresh ': effects) result (Int -> rest) where
+  run' = fmap run' . runFresh'
 
 instance Run '[] result result where
   run' = Eff.run
