1a65ba78135189f6b2fd68c224680262a5f644f1
Rob Rix <rob.rix@me.com>
2018-04-03 20:51:46 -0400

    Define a case for Exc effects.

--- src/Control/Monad/Effect/Run.hs
+++ src/Control/Monad/Effect/Run.hs
@@ -3,6 +3,7 @@ module Control.Monad.Effect.Run where
 
 import Control.Monad.Effect.Coroutine
 import Control.Monad.Effect.Embedded
+import Control.Monad.Effect.Exception
 import Control.Monad.Effect.Internal as Eff
 
 class Run effects result function | effects result -> function where
@@ -13,6 +14,9 @@ instance Run effects result rest => Run (Yield a b ': effects) result ((a -> b) 
 
 instance Run effects result rest => Run (Embedded effects ': effects) result rest where
   run' = run' . relay pure (\ (Embed e) k -> e >>= k)
+
+instance Run effects (Either exc result) rest => Run (Exc exc ': effects) result rest where
+  run' = run' . runError
 
 instance Run '[] result result where
   run' = Eff.run
