9fbf199ae2ae8e56c4ed3669cea18e87abdd2705
Rob Rix <rob.rix@me.com>
2018-04-03 20:47:00 -0400

    Define a case for Yield effects.

--- src/Control/Monad/Effect/Run.hs
+++ src/Control/Monad/Effect/Run.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE DataKinds, FlexibleContexts, FlexibleInstances, FunctionalDependencies, TypeOperators, UndecidableInstances #-}
 module Control.Monad.Effect.Run where
 
+import Control.Monad.Effect.Coroutine
 import Control.Monad.Effect.Internal as Eff
 
 class Run effects result function | effects result -> function where
   run' :: Eff effects result -> function
+
+instance Run effects result rest => Run (Yield a b ': effects) result ((a -> b) -> rest) where
+  run' = fmap run' . runCoro
 
 instance Run '[] result result where
   run' = Eff.run
