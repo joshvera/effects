0e294fdbb15a908153254a14c518aee83b84a29c
Rob Rix <rob.rix@me.com>
2018-04-03 20:45:53 -0400

    Stub in a Run typeclass.

--- src/Control/Monad/Effect/Run.hs
+++ src/Control/Monad/Effect/Run.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE DataKinds, FlexibleContexts, FlexibleInstances, FunctionalDependencies, TypeOperators, UndecidableInstances #-}
 module Control.Monad.Effect.Run where
+
+import Control.Monad.Effect.Internal as Eff
+
+class Run effects result function | effects result -> function where
+  run' :: Eff effects result -> function
