af8e6a1547a9a5127966da42498c99c5ef95fcfd
Rob Rix <rob.rix@me.com>
2018-06-09 13:40:47 -0400

    Comment out most of the Embedded machinery.

--- src/Control/Monad/Effect/Embedded.hs
+++ src/Control/Monad/Effect/Embedded.hs
@@ -12,10 +12,10 @@ module Control.Monad.Effect.Embedded (
   Embedded(..),
   Raisable(..),
   embed,
-  raiseEmbedded,
-  liftEmbedded,
-  runEmbedded,
-  runEmbeddedAsync
+  -- raiseEmbedded,
+  -- liftEmbedded,
+  -- runEmbedded,
+  -- runEmbeddedAsync
 ) where
 
 import Control.Concurrent.Async (async)
@@ -42,23 +42,23 @@ instance (Member e r, Raisable m r) =>  Raisable (e ': m) r where
       Right x -> send x
       Left u' -> raiseUnion u'
 
-raiseEmbedded :: (Raisable e e', Effectful m) => m e a -> m e' a
-raiseEmbedded = raiseHandler loop
-  where
-    loop (Val x)  = pure x
-    loop (E u' q) = raiseUnion u' >>= (q >>> loop)
-
-liftEmbedded :: (Raisable e e', Effectful m) => m (Embedded e ': e') a -> m e' a
-liftEmbedded = raiseHandler (runEmbedded void)
-
-runEmbedded :: (Raisable e e', Effectful m)
-            => (forall v. m e' v -> m e'' ())
-            -> m (Embedded e ': e'') a
-            -> m e'' a
-runEmbedded f = raiseHandler (relay pure (\(Embed e) -> (lowerEff (f (raiseEff (raiseEmbedded e))) >>=)))
-
-runEmbeddedAsync :: (Raisable e e', Member (Lift IO) e'', Effectful m)
-                 => (forall v. m e' v -> IO v)
-                 -> m (Embedded e ': e'') a
-                 -> m e'' a
-runEmbeddedAsync f = raiseHandler (runEmbedded (liftIO . void . async . f . raiseEff))
+-- raiseEmbedded :: (Raisable e e', Effectful m, Effect (Union e')) => m e a -> m e' a
+-- raiseEmbedded = raiseHandler loop
+--   where
+--     loop (Val x)  = pure x
+--     loop (E u' q) = raiseUnion u' >>= (q >>> loop)
+--
+-- liftEmbedded :: (Raisable e e', Effectful m) => m (Embedded e ': e') a -> m e' a
+-- liftEmbedded = raiseHandler (runEmbedded void)
+--
+-- runEmbedded :: (Raisable e e', Effectful m)
+--             => (forall v. m e' v -> m e'' ())
+--             -> m (Embedded e ': e'') a
+--             -> m e'' a
+-- runEmbedded f = raiseHandler (relay pure (\(Embed e) -> (lowerEff (f (raiseEff (raiseEmbedded e))) >>=)))
+--
+-- runEmbeddedAsync :: (Raisable e e', Member (Lift IO) e'', Effectful m)
+--                  => (forall v. m e' v -> IO v)
+--                  -> m (Embedded e ': e'') a
+--                  -> m e'' a
+-- runEmbeddedAsync f = raiseHandler (runEmbedded (liftIO . void . async . f . raiseEff))
