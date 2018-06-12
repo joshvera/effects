b4fb126a47807ee697653d4aac76ef88b673a51f
Rob Rix <rob.rix@me.com>
2018-06-12 10:53:12 -0400

    Rename the Throw constructor back to Resumable.

--- src/Control/Monad/Effect/Resumable.hs
+++ src/Control/Monad/Effect/Resumable.hs
@@ -12,11 +12,10 @@ module Control.Monad.Effect.Resumable
 import Control.Monad.Effect.Internal
 import Data.Functor.Classes
 
-data Resumable exc (m :: * -> *) a where
-  Throw :: exc a -> Resumable exc m a
+data Resumable exc (m :: * -> *) a = Resumable (exc a)
 
 throwResumable :: (Member (Resumable exc) e, Effectful m) => exc v -> m e v
-throwResumable = send . Throw
+throwResumable = send . Resumable
 
 catchResumable :: (Member (Resumable exc) e, Effectful m)
                => m e a
@@ -28,21 +27,21 @@ handleResumable :: (Member (Resumable exc) e, Effectful m)
                 => (forall v. exc v -> m e v)
                 -> m e a
                 -> m e a
-handleResumable handler = raiseHandler (interpose pure (\(Throw e) yield -> lowerEff (handler e) >>= yield))
+handleResumable handler = raiseHandler (interpose pure (\(Resumable e) yield -> lowerEff (handler e) >>= yield))
 
 
 runResumable :: (Effectful m, Effect (Union e)) => m (Resumable exc ': e) a -> m e (Either (SomeExc exc) a)
 runResumable = raiseHandler go
-  where go (Return a)           = pure (Right a)
-        go (Effect (Throw e) _) = pure (Left (SomeExc e))
-        go (Other u k)          = handleStateful (Right ()) (either (pure . Left) runResumable) u k
+  where go (Return a)               = pure (Right a)
+        go (Effect (Resumable e) _) = pure (Left (SomeExc e))
+        go (Other u k)              = handleStateful (Right ()) (either (pure . Left) runResumable) u k
 
 -- | Run a 'Resumable' effect in an 'Effectful' context, using a handler to resume computation.
 runResumableWith :: (Effectful m, Effect (Union effects)) => (forall resume . exc resume -> m (Resumable exc ': effects) resume) -> m (Resumable exc ': effects) a -> m effects a
 runResumableWith handler = go . lowerEff
-  where go (Return a)           = raiseEff (pure a)
-        go (Effect (Throw e) k) = runResumableWith handler (raiseEff (lowerEff (handler e) >>= k))
-        go (Other u k)          = handle (runResumableWith handler) u (raiseEff . k)
+  where go (Return a)               = raiseEff (pure a)
+        go (Effect (Resumable e) k) = runResumableWith handler (raiseEff (lowerEff (handler e) >>= k))
+        go (Other u k)              = handle (runResumableWith handler) u (raiseEff . k)
 
 
 data SomeExc exc where
