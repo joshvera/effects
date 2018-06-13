8708a64c1b3ccd44b0dc9b17f0ae7b0432c50537
Rob Rix <rob.rix@me.com>
2018-06-13 11:38:13 -0400

    Reformat the handleState signature.

--- src/Control/Monad/Effect/Internal.hs
+++ src/Control/Monad/Effect/Internal.hs
@@ -116,7 +116,11 @@ decomposeEff2 (E u q) = Right $ case decompose u of
   Right eff1 -> Right (Left (Request eff1 (apply q)))
 
 class Effect effect where
-  handleState :: Functor c => c () -> (forall x . c (Eff effects x) -> Eff effects' (c x)) -> Request effect (Eff effects) a -> Request effect (Eff effects') (c a)
+  handleState :: Functor c
+              => c ()
+              -> (forall x . c (Eff effects x) -> Eff effects' (c x))
+              -> Request effect (Eff effects) a
+              -> Request effect (Eff effects') (c a)
 
 liftStatefulHandler :: (Functor c, Effects effects') => c () -> (forall x . c (Eff effects x) -> Eff effects' (c x)) -> Union effects' (Eff effects) b -> Arrow (Eff effects) b a -> Eff effects' (c a)
 liftStatefulHandler c handler u k = fromRequest (handleState c handler (Request u k))
