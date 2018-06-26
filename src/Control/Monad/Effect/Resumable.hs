7dd58cfa171ac3f8d9f816e72ddb428e86f8be77
Rob Rix <rob.rix@me.com>
2018-06-26 11:56:41 -0400

    Rename Throw back to Resumable.

--- src/Control/Monad/Effect/Resumable.hs
+++ src/Control/Monad/Effect/Resumable.hs
@@ -10,21 +10,20 @@ module Control.Monad.Effect.Resumable
 import Control.Monad.Effect.Internal
 import Data.Functor.Classes
 
-data Resumable exc (m :: * -> *) a where
-  Throw :: exc a -> Resumable exc m a
+newtype Resumable exc (m :: * -> *) a = Resumable (exc a)
 
 throwResumable :: (Member (Resumable exc) e, Effectful m) => exc v -> m e v
-throwResumable = send . Throw
+throwResumable = send . Resumable
 
 runResumable :: (Effectful m, Effect (Union e)) => m (Resumable exc ': e) a -> m e (Either (SomeExc exc) a)
 runResumable = raiseHandler go
   where go (Return a)           = pure (Right a)
-        go (Effect (Throw e) _) = pure (Left (SomeExc e))
+        go (Effect (Resumable e) _) = pure (Left (SomeExc e))
         go (Other u k)          = liftStatefulHandler (Right ()) (either (pure . Left) runResumable) u k
 
 -- | Run a 'Resumable' effect in an 'Effectful' context, using a handler to resume computation.
 runResumableWith :: (Effectful m, Effect (Union effects)) => (forall resume . exc resume -> m effects resume) -> m (Resumable exc ': effects) a -> m effects a
-runResumableWith handler = interpret (\ (Throw e) -> handler e)
+runResumableWith handler = interpret (\ (Resumable e) -> handler e)
 
 
 data SomeExc exc where
@@ -38,4 +37,4 @@ instance (Show1 exc) => Show (SomeExc exc) where
 
 
 instance Effect (Resumable exc) where
-  handleState c dist (Request (Throw exc) k) = Request (Throw exc) (dist . (<$ c) . k)
+  handleState c dist (Request (Resumable exc) k) = Request (Resumable exc) (dist . (<$ c) . k)
